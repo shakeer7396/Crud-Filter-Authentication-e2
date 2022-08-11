97d5a170c18dc2bf8fcc46cfaffa83408cf41427
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-08-11 08:23:57 +0530

    v

--- src/Components/ReqAuth.jsx
+++ src/Components/ReqAuth.jsx
@@ -1,4 +1,11 @@
 //Create the HOC for protected Routes
-const ReqAuth = () => {};
+const ReqAuth = ({children}) => {
+
+    const auth= useSelector((store)=>store.AuthReducer.isAuth)
+    if(!auth){
+        //login page
+        return <Navigate to='/login' state={{from:location}}
+    }
+};
 
 export default ReqAuth;
