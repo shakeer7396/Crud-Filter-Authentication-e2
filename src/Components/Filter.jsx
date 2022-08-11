bc276443d86f7db45e2ee9f32374427c87b41d4c
shakeer7396 <97457588+shakeer7396@users.noreply.github.com>
2022-08-11 13:00:48 +0530

    filter

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,22 +1,54 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
+import {useSearchParams} from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { getWatchesdata } from "../Redux/AppReducer/action";
+
 
 const Filter = () => {
   // DO NOT CHANGE THE ORDER of the category filters: ie. Analog, Digital, Chronograph in the UI
+
+  const [Searchparams,SetSearchparams]=useSearchParams();
+  const [category,SetCategory] =useState(Searchparams.getAll('category')||[]);
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    SetSearchparams({category:category})
+    let categoryparams={
+      params:{
+        category:Searchparams.getAll("category")
+      }
+    }
+    dispatch(getWatchesdata(categoryparams))
+  },[category,Searchparams,dispatch,SetSearchparams])
+console.log(category);
+
+  const handleCheckbox =(e)=>{
+    const point=e.target.value;
+    let newList=[...category];
+    if(category.includes(point)){
+      newList.splice(newList.indexOf(point),1)
+    }
+    else{
+      newList.push(point);
+    }
+    SetCategory(newList)
+
+  }
   return (
     <div>
       <h3>Filters</h3>
       <div>Category</div>
       <div data-testid="filter-category">
         <div>
-          <input type="checkbox" value="Analog" />
+          <input type="checkbox" value="Analog" onChange={handleCheckbox} defaultChecked={category.includes('Analog')} />
           <label>Analog</label>
         </div>
         <div>
-          <input type="checkbox" value="Digital" />
+          <input type="checkbox" value="Digital" onChange={handleCheckbox} defaultChecked={category.includes('Digital')} />
           <label>Digital</label>
         </div>
         <div>
-          <input type="checkbox" value="Chronograph" />
+          <input type="checkbox" value="Chronograph" onChange={handleCheckbox} defaultChecked={category.includes('Chronography')} />
           <label>Chronograph</label>
         </div>
       </div>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import {useNavigate} from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate=useNavigate();
   return (
     <div data-testid="navbar">
       <div data-testid="navbar-home-link">
@@ -9,12 +11,13 @@ const Navbar = () => {
           width="60px"
           alt="logo"
           style={{ display: "block" }}
+          onClick={()=>navigate('/')}
         />
       </div>
 
       <div>
         {/* Link button to /login page, if the user is not authenticated, else don't show it*/}
-        <button data-testid="navbar-login-button">LOGIN</button>
+        <button data-testid="navbar-login-button" onClick={()=>navigate('/login')} >LOGIN</button>
       </div>
     </div>
   );
